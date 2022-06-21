3f587b4f25d729515d7b66c97a7f55ab95d2fdf0
bapcai <phuochuy1328@gmail.com>
2022-06-21 20:51:51 +0700

    header#2 finished

--- src/components/Layouts/AccountItem/index.js
+++ src/components/Layouts/AccountItem/index.js
@@ -11,7 +11,7 @@ function AccountItem({ props }) {
         <div className={cx('wrapper')}>
             <img
                 className={cx('avatar')}
-                src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/068e10f0c70ce8e96693d822a2141795~c5_300x300.webp?x-expires=1655805600&x-signature=shJscYZcHH4kikD9FJWkaG9vY9E%3D"
+                src="https://scontent.fsgn5-2.fna.fbcdn.net/v/t39.30808-6/277226796_317030630521933_2223863444420889595_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Dwuisd-RwiQAX_haQ5I&_nc_ht=scontent.fsgn5-2.fna&oh=00_AT9Zo6yeLmYJG0nK4CfHA8w-nDoDXtYu6QZreEKHNzMOVg&oe=62B756B0"
                 alt="account-name"
             />
             <div className={cx('info')}>
